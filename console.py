0b155ac30f1a04b2a4fb9ff11228f6d7ad8941fc
MahderZergaw <mahderzergaw640@gmail.com>
2023-10-15 10:53:57 -0700

    prin

--- console.py
+++ console.py
@@ -33,9 +33,9 @@ class HBNBCommand(cmd.Cmd):
         it (to the JSON file) and prints the id
         """
         if args == "" or args is None:
-            print("* class name missing *")
+            print("** class name missing **")
         elif args not in storage.classes():
-            print("** class doesn't exist")
+            print("** class doesn't exist **")
         else:
             model = storage.classes()[args]()
             model.save()
@@ -47,34 +47,34 @@ class HBNBCommand(cmd.Cmd):
         based on the class name and id
         """
         if args == "" or args is None:
-            print("* class name missing *")
+            print("** class name missing **")
         else:
             args_list = args.split(' ')
             if args_list[0] not in storage.classes():
-                print("* class doesn't exist *")
+                print("** class doesn't exist **")
             elif len(args_list) < 2:
-                print("* instance id missing *")
+                print("** instance id missing **")
             else:
                 key = "{}.{}".format(args_list[0], args_list[1])
                 if key not in storage.all():
-                    print("* no instance found *")
+                    print("** no instance found **")
                 else:
                     print(storage.all()[key])
 
     def do_destroy(self, args):
         """Deletes an instance based on the class name and id"""
         if args == "" or args is None:
-            print("* class name missing *")
+            print("** class name missing **")
         else:
             args_list = args.split(' ')
             if args_list[0] not in storage.classes():
-                print("* class doesn't exist *")
+                print("** class doesn't exist **")
             elif len(args_list) < 2:
-                print("* instance id missing *")
+                print("** instance id missing **")
             else:
                 obj_id = "{}.{}".format(args_list[0], args_list[1])
                 if obj_id not in storage.all():
-                    print("* no instance found *")
+                    print("** no instance found **")
                 else:
                     del storage.all()[obj_id]
                     storage.save()
@@ -86,7 +86,7 @@ class HBNBCommand(cmd.Cmd):
         if args != "":
             args_list = args.split(' ')
             if args_list[0] not in storage.classes():
-                print("* class doesn't exist *")
+                print("** class doesn't exist **")
             else:
                 str_rep = [str(value) for key,
                            value in storage.all().items()
@@ -100,20 +100,20 @@ class HBNBCommand(cmd.Cmd):
     def do_update(self, args):
         """Updates an instance based on the class name and id"""
         if not args:
-            print("* class name missing *")
+            print("** class name missing **")
             return
         args_list = args.split()
         if args_list[0] != "BaseModel":
-            print("* class doesn't exist *")
+            print("** class doesn't exist **")
             return
         if len(args_list) < 2 or self.id is None:
-            print("* instance id missing *")
+            print("** instance id missing **")
             return
         if len(args_list) < 3:
-            print("* attribute name missing *")
+            print("** attribute name missing **")
             return
         if len(args_list) < 4:
-            print("* value mising *")
+            print("** value mising **")
             return
 
         class_name = args_list[0]
@@ -123,7 +123,7 @@ class HBNBCommand(cmd.Cmd):
         all_objs = storage.all()
         key = "{}.{}".format(class_name, instance_id)
         if key not in all_objs:
-            print("* no instance found *")
+            print("** no instance found **")
         else:
             obj = all_objs[key]
             setattr(obj, attribute_name, attribute_value)
